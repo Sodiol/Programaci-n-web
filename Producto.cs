4296c7b53d4ba52979d9c3340cd05790b43da7c5
Sodiol <91626926+Sodiol@users.noreply.github.com>
2024-04-15 10:48:05 -0600

    Add files via upload
    Examen de segunda parcial de programación web de 4° semestre

---
+++ Producto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Examen_1P.Data
+{
+    public class Producto
+    {
+
+        [Required(ErrorMessage = "Debe ser entero positivo mayor a 0")]
+        [Range(1, int.MaxValue, ErrorMessage = "El número de inventario debe ser un número entero positivo.")]
+        public int NumeroInventario { get; set; }
+
+        [Required(ErrorMessage = "El nombre no debe ser vacío")]
+            public string Nombre { get; set; }
+
+        [Required(ErrorMessage ="Debe seleccionar departamento")]
+            public string? Departamento { get; set; }
+        
+
+    }
+}
